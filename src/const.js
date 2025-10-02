8463a3324e7334e923aa4e7a6dc26a303d82201f
Takashi Mori <kanetugu2015@gmail.com>
2025-10-03 06:30:49 +0900

    add drone

--- src/const.js
+++ src/const.js
@@ -10,3 +10,14 @@ export const SYSTEM_CONTROL_SERVICE_NAME = 'Service/SystemControl';
 export const ASSET_NAME  = 'HakoScrachClient';
 export const CLIENT_NAME = 'HakoScrachClient';
 export const DELTA_TIME_USEC = 1_000_000;
+
+export const DRONE_SERVICE = {
+  SET_READY   : 'DroneService/DroneSetReady',
+  TAKEOFF     : 'DroneService/DroneTakeOff',
+  LAND        : 'DroneService/DroneLand',
+  GET_STATE   : 'DroneService/DroneGetState',
+  GO_TO       : 'DroneService/DroneGoTo',
+  CAMERA_TILT : 'DroneService/CameraSetTilt',
+  MAGNET_GRAB : 'DroneService/MagnetGrab',
+  // LiDAR や CameraCapture は後で追加
+};
